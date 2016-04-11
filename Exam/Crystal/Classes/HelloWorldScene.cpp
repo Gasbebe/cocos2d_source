b412c097092f694b7103f00f7efcb25356eb41e4
LHH <gasbebe@gmail.com>
2016-04-11 17:06:03 +0900

    Crystal

---
+++ Exam/Crystal/Classes/HelloWorldScene.cpp
@@ -0,0 +1,78 @@
+﻿#include "HelloWorldScene.h"
+
+USING_NS_CC;
+using namespace CocosDenshion;
+
+Scene* HelloWorld::createScene()
+{
+    auto scene = Scene::create();   
+    auto layer = HelloWorld::create();
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+
+    if ( !LayerColor::initWithColor(Color4B(255,255,255,255)) )
+    {
+        return false;
+    }
+    
+    /////////////////////////////
+	winSize = Director::getInstance()->getWinSize();
+
+	auto bg = Sprite::create("Images/background.png");
+	bg->setPosition(Vec2(winSize.width / 2, winSize.height / 2));
+	this->addChild(bg);
+
+	auto logo = Sprite::create("Images/logo.png");
+	logo->setPosition(Vec2(winSize.width / 2, winSize.height / 2 + 200));
+	logo->setScale(0.8f);
+	this->addChild(logo);
+
+	auto starParticles = ParticleSystem::create("particles/bg-stars.plist");
+	this->addChild(starParticles);
+	
+	auto playMenuItem = MenuItemImage::create("Images/btn-play.png",
+												"Images/btn-play-down.png",
+												CC_CALLBACK_0(HelloWorld::onPressPlay, this));
+
+	playMenuItem->setPosition(Vec2(winSize.width / 2, winSize.height / 2-100));
+
+	auto aboutMenuItem = MenuItemImage::create("Images/btn-about.png",
+												"Images/btn-about-down.png",
+												CC_CALLBACK_0(HelloWorld::onPressPlay, this));
+
+	aboutMenuItem->setPosition(Vec2(winSize.width / 2, winSize.height / 2 -300));
+
+	auto menu = Menu::create(playMenuItem, aboutMenuItem, nullptr);
+	menu->setPosition(Vec2(0, 0));
+	this->addChild(menu);
+	
+	int menuSelection = kMenuSelectionPlay;
+	
+	auto act = Sequence::create(ScaleBy::create(0.3, 0), ScaleTo::create(0.3, 0.8),
+		ScaleTo::create(0.2, 0.7), ScaleTo::create(0.2, 0.8),nullptr);
+
+	logo->runAction(act->clone());
+	playMenuItem->runAction(act->clone());
+	aboutMenuItem->runAction(act->clone());
+
+	//sound
+	SimpleAudioEngine::getInstance()->preloadBackgroundMusic("sounds/loop.mp3");
+	SimpleAudioEngine::getInstance()->setBackgroundMusicVolume(1.0f);
+	SimpleAudioEngine::getInstance()->playBackgroundMusic("sounds/loop.mp3", true);
+
+    return true;
+}
+void HelloWorld::onPressPlay() {
+
+}
+
+//
+// MainScene class
+//
---
+++ Exam/Crystal/Classes/HelloWorldScene.h
@@ -0,0 +1,27 @@
+﻿#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+#include "SimpleAudioEngine.h"
+#include "cocos2d.h"
+
+class HelloWorld : public cocos2d::LayerColor
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+	
+	enum {
+		kMenuSelectionPlay = 1,
+		kMenuSelectionAbout = 2,
+		
+	};
+	//0으로 초기화
+	cocos2d::Size winSize;
+    CREATE_FUNC(HelloWorld);
+	void onPressPlay();
+	void onPressAbout();
+	void onAnimationComplete();
+	void onUpdate();
+};
+
+#endif // __HELLOWORLD_SCENE_H__
